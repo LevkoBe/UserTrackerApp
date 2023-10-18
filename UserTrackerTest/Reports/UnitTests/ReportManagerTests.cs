2593b2fb72bed5fac9a9d6729d931116bae6db56
LevkoBe <lbeniakh@kse.org.ua>
2023-10-18 14:43:23 +0300

    Unit tests for report classes#36

---
+++ UserTrackerTest/Reports/UnitTests/ReportManagerTests.cs
@@ -0,0 +1,89 @@
+﻿namespace UserTracker
+{
+
+    public class ReportManagerTests
+    {
+        [Fact]
+        public void Expect_NewReport_When_CreateReport()
+        {
+            // Arrange
+            var reportManager = new ReportManager();
+            var reportConfig = new ReportConfiguration
+            {
+                Metrics = new List<string> { "dailyAverage", "total" },
+                UserNicknames = new List<string> { "user1", "user2" }
+            };
+
+            // Act
+            bool result = reportManager.CreateReport("SampleReport", reportConfig);
+
+            // Assert
+            Assert.True(result);
+            var reports = reportManager.GetReports();
+            Assert.Single(reports);
+            Assert.Equal("SampleReport", reports[0].ReportName);
+        }
+
+        [Fact]
+        public void Expect_OneReport_When_TwoIdenticalCreated()
+        {
+            // Arrange
+            var reportManager = new ReportManager();
+            var reportConfig1 = new ReportConfiguration
+            {
+                Metrics = new List<string> { "dailyAverage", "total" },
+                UserNicknames = new List<string> { "user1", "user2" }
+            };
+            var reportConfig2 = new ReportConfiguration
+            {
+                Metrics = new List<string> { "weeklyAverage", "total" },
+                UserNicknames = new List<string> { "user3", "user4" }
+            };
+
+            // Act
+            bool result1 = reportManager.CreateReport("SampleReport", reportConfig1);
+            bool result2 = reportManager.CreateReport("SampleReport", reportConfig2);
+
+            // Assert
+            Assert.True(result1);
+            Assert.False(result2);
+            var reports = reportManager.GetReports();
+            Assert.Single(reports);
+            Assert.Equal("SampleReport", reports[0].ReportName);
+        }
+
+        [Fact]
+        public void Expect_Report_When_GetReport()
+        {
+            // Arrange
+            var reportManager = new ReportManager();
+            var reportConfig = new ReportConfiguration
+            {
+                Metrics = new List<string> { "dailyAverage", "total" },
+                UserNicknames = new List<string> { "user1", "user2" }
+            };
+            reportManager.CreateReport("SampleReport", reportConfig);
+
+            // Act
+            var report = reportManager.GetReport("SampleReport");
+
+            // Assert
+            Assert.NotNull(report);
+            Assert.Equal("SampleReport", report.ReportName);
+        }
+
+        [Fact]
+        public void Expect_Null_When_GetReportThatDoesntExist()
+        {
+            // Arrange
+            var reportManager = new ReportManager();
+
+            // Act
+            var report = reportManager.GetReport("NonExistentReport");
+
+            // Assert
+            Assert.Null(report);
+        }
+    }
+}
+
---
+++ UserTrackerTest/Reports/UnitTests/ReportTests.cs
@@ -0,0 +1,23 @@
+﻿namespace UserTracker.Tests
+{
+    public class ReportTests
+    {
+        [Fact]
+        public void Expect_ProperProperties_When_ReportIsCreated()
+        {
+            // Arrange
+            string reportName = "SampleReport";
+            var metrics = new List<string> { "dailyAverage", "total" };
+            var userNicknames = new List<string> { "user1", "user2" };
+
+            // Act
+            var report = new Report(reportName, metrics, userNicknames);
+
+            // Assert
+            Assert.Equal(reportName, report.ReportName);
+            Assert.Equal(metrics, report.Metrics);
+            Assert.Equal(userNicknames, report.UserNicknames);
+        }
+    }
+}
+
